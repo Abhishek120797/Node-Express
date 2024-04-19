7475df39bf6f34c40fb1032a05ba58fea8b4c040
Abhishek120797 <abhi.jais1211@gmail.com>
2024-04-20 04:55:34 +0530

    Changed Sequelize logging false

--- Edu-Connect-API/src/db/dbInstance.js
+++ Edu-Connect-API/src/db/dbInstance.js
@@ -4,7 +4,7 @@ import { db_name } from "../constants.js";
 const sequelize = new Sequelize(
   `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}/${db_name}?sslmode=require`,
   {
-    logging: true,
+    logging: false,
   }
 );
 
