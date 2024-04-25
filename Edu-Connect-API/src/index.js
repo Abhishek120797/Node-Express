8c2b197eb891105311a10b85a1bb2a4d7d3ac849
Abhishek120797 <abhi.jais1211@gmail.com>
2024-04-25 16:46:32 +0530

    index.js updated

--- Edu-Connect-API/src/index.js
+++ Edu-Connect-API/src/index.js
@@ -1,7 +1,5 @@
-// import dotenv from "dotenv";
-// dotenv.config({ path: "./env" });
-
-require("dotenv").config({ path: "./.env" });
+import dotenv from "dotenv";
+dotenv.config({ path: "./env" });
 
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
