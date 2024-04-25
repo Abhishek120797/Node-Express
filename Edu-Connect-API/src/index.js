40665c4d7f26ba7598a7932abad0f2a95914dcce
Abhishek120797 <abhi.jais1211@gmail.com>
2024-04-25 16:39:46 +0530

    .yml file or index.js file updated to resolve error of pm2

--- Edu-Connect-API/src/index.js
+++ Edu-Connect-API/src/index.js
@@ -1,5 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./env" });
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./env" });
+
+require("dotenv").config({ path: "./.env" });
 
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
